8e9e5c0ed7136f01d984d94a64bc40a162f83333
bxkr <bxkr@mail.com>
2022-03-27 12:03:46 +0300

    Simple weeks recyclerView

--- app/src/main/java/org/bxkr/octodiary/MainActivity.kt
+++ app/src/main/java/org/bxkr/octodiary/MainActivity.kt
@@ -3,12 +3,20 @@ package org.bxkr.octodiary
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import org.bxkr.octodiary.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var token: String? = null
+    private var userId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +26,34 @@ class MainActivity : AppCompatActivity() {
         val sharedPref =
             this.getSharedPreferences(getString(R.string.auth_file_key), Context.MODE_PRIVATE)
                 ?: return
-        val token = sharedPref.getString(getString(R.string.token), null)
-        val uid = sharedPref.getString(getString(R.string.user_id), null)
-        if (token == null && uid == null) {
+        token = sharedPref.getString(getString(R.string.token), null)
+        userId = sharedPref.getString(getString(R.string.user_id), null)
+        if (token == null && userId == null) {
             startActivity(Intent(this, LoginActivity::class.java))
         } else {
-            binding.textView.text = getString(R.string.user_id_is, uid)
+            createDiary()
         }
+    }
+
+    private fun createDiary() {
+        val call = NetworkService.api().diary(token, userId)
+        call.enqueue(object : Callback<NetworkService.Diary> {
+            override fun onResponse(
+                call: Call<NetworkService.Diary>,
+                response: Response<NetworkService.Diary>
+            ) = if (response.isSuccessful) {
+                val diaryData = response.body()!!.weeks
+                binding.weeks.layoutManager = LinearLayoutManager(this@MainActivity)
+                binding.weeks.adapter = WeeksAdapter(this@MainActivity, diaryData)
+                binding.swipeRefresh.setOnRefreshListener { createDiary() }
+            } else {
+                Snackbar.make(binding.root, R.string.out_of_date, Snackbar.LENGTH_LONG).show()
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+            }
+
+            override fun onFailure(call: Call<NetworkService.Diary>, t: Throwable) {
+                Log.e(this::class.simpleName, getString(R.string.retrofit_error))
+            }
+        })
     }
 }
--- app/src/main/java/org/bxkr/octodiary/NetworkService.kt
+++ app/src/main/java/org/bxkr/octodiary/NetworkService.kt
@@ -3,6 +3,8 @@ package org.bxkr.octodiary
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 import java.math.BigInteger
@@ -11,7 +13,93 @@ object NetworkService {
     private const val apiUrl =
         "https://api.bxkr.org/school/" // See https://github.com/OctoDiary/OctoDiary-API
 
-    class AuthResult(val access_token: String, val user_id: BigInteger)
+    data class AuthResult(
+        val access_token: String, val user_id: BigInteger
+    )
+
+    data class Diary(
+        val weeks: List<Week>
+    )
+
+    data class Week(
+        val days: List<Day>,
+        val firstWeekDayDate: String,
+        val homeworksCount: Int,
+        val id: String,
+        val lastWeekDayDate: String
+    )
+
+    data class Day(
+        val date: String,
+        val dayHomeworksProgress: DayHomeworksProgress,
+        val hasImportantWork: Boolean,
+        val lessons: List<Lesson>,
+        val messengerEntryPoint: Any
+    )
+
+    data class DayHomeworksProgress(
+        val completedLessonsWithHomeworksCount: Int,
+        val totalLessonsWithHomeworksCount: Int
+    )
+
+    data class Lesson(
+        val comment: Any,
+        val endDateTime: String,
+        val group: Group,
+        val hasAttachment: Boolean,
+        val homework: Homework,
+        val id: Long,
+        val importantWorks: List<String>,
+        val isCanceled: Boolean,
+        val isEmpty: Any,
+        val number: Int,
+        val place: Any,
+        val startDateTime: String,
+        val subject: Subject,
+        val teacher: Teacher,
+        val theme: String,
+        val workMarks: List<WorkMark>
+    )
+
+    data class Group(
+        val id: Long,
+        val name: String,
+        val parentId: Any,
+        val parentName: Any
+    )
+
+    data class Homework(
+        val attachments: List<Any>,
+        val isCompleted: Boolean,
+        val text: String,
+        val workIsAttachRequired: Boolean
+    )
+
+    data class Subject(
+        val id: Long,
+        val knowledgeArea: String,
+        val name: String,
+        val subjectMood: Any
+    )
+
+    data class Teacher(
+        val avatarUrl: String,
+        val firstName: String,
+        val lastName: String,
+        val middleName: String,
+        val personId: Int
+    )
+
+    data class WorkMark(
+        val marks: List<Mark>,
+        val workId: Long
+    )
+
+    data class Mark(
+        val id: Long,
+        val mood: String,
+        val value: String
+    )
 
     interface API {
         @POST("auth")
@@ -19,6 +107,12 @@ object NetworkService {
             @Query("username") username: String?,
             @Query("password") password: String?,
         ): Call<AuthResult>
+
+        @GET("diary")
+        fun diary(
+            @Header("Access-Token") access_token: String?,
+            @Header("User-ID") user_id: String?,
+        ): Call<Diary>
     }
 
     fun api(): API {
