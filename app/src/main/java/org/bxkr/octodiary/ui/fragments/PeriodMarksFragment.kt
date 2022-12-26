622a473e0896c9a1f2706dfd4945ec1e718be79f
Andrey <rielyalkov1@gmail.com>
2022-12-26 20:56:36 +0300

    Catch error when data is out of date in PeriodMarksFragment + fix path in ic_round_timeline_24.xml

--- app/src/main/java/org/bxkr/octodiary/ui/fragments/PeriodMarksFragment.kt
+++ app/src/main/java/org/bxkr/octodiary/ui/fragments/PeriodMarksFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.preference.PreferenceManager
 import org.bxkr.octodiary.R
+import org.bxkr.octodiary.Utils
 import org.bxkr.octodiary.databinding.FragmentPeriodMarksBinding
 import org.bxkr.octodiary.models.periodmarks.PeriodMarksResponse
 import org.bxkr.octodiary.ui.activities.MainActivity
@@ -20,6 +21,19 @@ class PeriodMarksFragment :
         mainActivity = activity as MainActivity
         mainActivity.title = getString(R.string.period_marks)
         preferences = PreferenceManager.getDefaultSharedPreferences(mainActivity)
-        periodMarks = mainActivity.periodMarksData!!
+        if (Utils.isSchoolDataOutOfDate(mainActivity)) {
+            mainActivity.let { it ->
+                it.binding.swipeRefresh.let { swipeRefresh ->
+                    it.createDiary {
+                        swipeRefresh.isRefreshing = false
+                        onViewCreated(view, savedInstanceState)
+                    }
+                    swipeRefresh.isRefreshing = true
+                    return@onViewCreated
+                }
+            }
+        } else {
+            periodMarks = mainActivity.periodMarksData!!
+        }
     }
 }
