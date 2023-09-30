333df6484d389f8df4df4ec6b86e880c058cd2f9
bxkr <rielyalkov1@gmail.com>
2023-09-30 21:04:34 +0300

    Bump to 18 (2.0.0-alpha01)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 18
-        versionName = "2.0.0"
+        versionName = "2.0.0-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
