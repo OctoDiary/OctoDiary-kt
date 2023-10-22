bc0acf35d169d7e19b1903c2f41b7fb4eb4b321a
bxkr <rielyalkov1@gmail.com>
2023-10-22 14:29:50 +0300

    Design ClassInfo

--- app/src/main/java/org/bxkr/octodiary/components/profile/ClassInfo.kt
+++ app/src/main/java/org/bxkr/octodiary/components/profile/ClassInfo.kt
@@ -1,8 +1,147 @@
 package org.bxkr.octodiary.components.profile
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.bxkr.octodiary.DataService
+import org.bxkr.octodiary.R
+import org.bxkr.octodiary.screens.navsections.RankingList
 
 @Composable
 fun ClassInfo() {
-
+    var showRanking by remember { mutableStateOf(false) }
+    val enterTransition1 = remember {
+        slideInHorizontally(
+            tween(200)
+        ) { it }
+    }
+    val exitTransition1 = remember {
+        slideOutHorizontally(
+            tween(200)
+        ) { it }
+    }
+    val enterTransition2 = remember {
+        slideInHorizontally(
+            tween(200)
+        ) { -it }
+    }
+    val exitTransition2 = remember {
+        slideOutHorizontally(
+            tween(200)
+        ) { -it }
+    }
+    Box {
+        AnimatedVisibility(
+            visible = showRanking,
+            enter = enterTransition1,
+            exit = exitTransition1
+        ) {
+            Column {
+                IconButton(onClick = { showRanking = false }) {
+                    Icon(Icons.AutoMirrored.Rounded.ArrowBack, stringResource(id = R.string.back))
+                }
+                RankingList()
+            }
+        }
+        AnimatedVisibility(
+            visible = !showRanking,
+            enter = enterTransition2,
+            exit = exitTransition2
+        ) {
+            Column(Modifier.padding(16.dp)) {
+                with(DataService) {
+                    Text(
+                        "Класс ${profile.children[0].className}",
+                        style = MaterialTheme.typography.titleMedium
+                    ) // FUTURE: USES_FIRST_CHILD UNTRANSLATED
+                    Text("${classMembers.size} ученика") // FUTURE: UNTRANSLATED NEED_PLURAL
+                    Row(
+                        Modifier
+                            .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                            .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            "имя", // FUTURE: UNTRANSLATED
+                            Modifier.alpha(.8f),
+                            style = MaterialTheme.typography.labelLarge
+                        )
+                        Text(
+                            "рейтинг", // FUTURE: UNTRANSLATED
+                            Modifier.alpha(.8f),
+                            style = MaterialTheme.typography.labelLarge
+                        )
+                    }
+                    LazyColumn(
+                        Modifier.clip(MaterialTheme.shapes.large),
+                        verticalArrangement = Arrangement.spacedBy(2.dp)
+                    ) {
+                        items(classMembers) {
+                            Card(
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceContainerLowest
+                                ),
+                                shape = MaterialTheme.shapes.extraSmall
+                            ) {
+                                Row(
+                                    Modifier
+                                        .fillMaxWidth()
+                                        .padding(16.dp),
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Column {
+                                        Text(
+                                            it.user.lastName,
+                                            style = MaterialTheme.typography.titleMedium
+                                        )
+                                        Text(it.user.run { "$firstName $middleName" })
+                                    }
+                                    FilledTonalIconButton(
+                                        onClick = { showRanking = true },
+                                        shape = MaterialTheme.shapes.small
+                                    ) {
+                                        Text(
+                                            ranking.firstOrNull { rankingMember -> it.personId == rankingMember.personId }
+                                                ?.rank?.rankPlace?.toString()
+                                                ?: "?",
+                                            style = MaterialTheme.typography.labelLarge
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
