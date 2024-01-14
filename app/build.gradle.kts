139b234f2aa6705a6cbec7e5231d3374a608639e
bxkr <rielyalkov1@gmail.com>
2024-01-14 13:54:08 +0300

    Add glance dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,8 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.glide)
     implementation(libs.converter.scalars)
+    implementation(libs.androidx.glance.appwidget)
+    implementation(libs.androidx.glance.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
