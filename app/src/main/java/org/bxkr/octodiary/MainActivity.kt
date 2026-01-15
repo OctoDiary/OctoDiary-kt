e48f465d92028c45dbba765e02bc27b1900d5ad1
Morkovka21Vek <morkovka21vek@gmail.com>
2026-01-15 16:33:50 +0300

    Fixed a line break error in the navigation bar

--- app/src/main/java/org/bxkr/octodiary/MainActivity.kt
+++ app/src/main/java/org/bxkr/octodiary/MainActivity.kt
@@ -75,6 +75,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -464,7 +465,10 @@ class MainActivity : FragmentActivity() {
                                 Icon(it.icon, stringResource(id = it.title))
                             },
                             label = {
-                                Text(stringResource(id = it.title))
+                                Text(stringResource(id = it.title),
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis
+                                )
                             })
                     }
                 }
